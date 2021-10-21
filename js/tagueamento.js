229bb31b4630da902cc6d4c97150e8a2b4588d54
Thiago <copocaneta@gmail.com>
2021-10-21 14:46:54 -0300

    Removendo console.logs

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -36,13 +36,13 @@ const btMenuDownloadPdf = document.querySelector('.menu-lista-download');
 
 // criando eventListener para capturar o clique no menu "Entre em Contato""
 btMenuentraEmContato.addEventListener('click',  () => {
-	console.log('clicou no entre em contato');
+	// console.log('clicou no entre em contato');
 	envioDeEvento('menu', 'entre_em_contato', 'link_externo')
 });
 
 // criando eventListener para capturar o clique no menu "Download PDF""
 btMenuDownloadPdf.addEventListener('click', () => {
-	console.log('clicou no download PDF');
+	// console.log('clicou no download PDF');
 	envioDeEvento('menu', 'download_pdf', 'download_pdf');
 });
 
